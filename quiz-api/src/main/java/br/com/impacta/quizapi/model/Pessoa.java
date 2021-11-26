ef50e934269026a76c82c74849e4e2ec0637d87c
GeilsonAraujo <geilsonopovo@gmail.com>
2021-11-25 21:51:07 -0300

    Adc Equals e HashCode

--- quiz-api/src/main/java/br/com/impacta/quizapi/model/Pessoa.java
+++ quiz-api/src/main/java/br/com/impacta/quizapi/model/Pessoa.java
@@ -1,8 +1,31 @@
 package br.com.impacta.quizapi.model;
 
+import java.util.Objects;
+
 public class Pessoa {
 	
 	private String nome;
+	private int idade;
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(idade, nome);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pessoa other = (Pessoa) obj;
+		return idade == other.idade && Objects.equals(nome, other.nome);
+	}
+
+
+	
 	public int getIdade() {
 		return idade;
 	}
@@ -11,7 +34,7 @@ public class Pessoa {
 		this.idade = idade;
 	}
 
-	private int idade;
+
 
 	public String getNome() {
 		return nome;
